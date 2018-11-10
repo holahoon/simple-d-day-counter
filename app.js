7b783b9e05028a9ab77537f5e5ecdbd6f9bb596e
rtdk0324 <42850638+rtdk0324@users.noreply.github.com>
2018-11-09 23:41:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ function countDown() {
   );
   let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
+// Makes the clock numbers to be two digit number
+  if (days < 10) {
+    days = "0" + days;
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
   document.getElementById("day").innerHTML = days;
   document.getElementById("hour").innerHTML = hours;
   document.getElementById("minute").innerHTML = minutes;
